ad60dda3d4b5db261e64d148e0af6b8b276b27a5
Julie Rezazadeh <rezazadehjulie@gmail.com>
2017-10-17 21:06:38 -0400

    Add files via upload

--- views/js/main.js
+++ views/js/main.js
@@ -524,7 +524,7 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 8;
+  var cols = 5;
   var s = 256;
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
