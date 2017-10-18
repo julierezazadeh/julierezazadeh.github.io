3f9f806c46eb3f62b0cfb82cf1dd53ca4d9f7ff0
Julie Rezazadeh <rezazadehjulie@gmail.com>
2017-10-17 23:09:58 -0400

    Add files via upload

--- views/js/main.js
+++ views/js/main.js
@@ -406,13 +406,14 @@ var resizePizzas = function(size) {
   function changeSliderLabel(size) {
     switch(size) {
       case "1":
-        document.querySelector("#pizzaSize").innerHTML = "Small";
+	  // Changed querySelector to getElementById because https://jsperf.com/getelementbyid-vs-queryselector/25 I wanted to see if this would allow the program to run faster, and this website proved that it would
+        document.getElementById("#pizzaSize").innerHTML = "Small";
         return;
       case "2":
-        document.querySelector("#pizzaSize").innerHTML = "Medium";
+        document.getElementById("#pizzaSize").innerHTML = "Medium";
         return;
       case "3":
-        document.querySelector("#pizzaSize").innerHTML = "Large";
+        document.getElementById("#pizzaSize").innerHTML = "Large";
         return;
       default:
         console.log("bug in changeSliderLabel");
@@ -424,7 +425,7 @@ var resizePizzas = function(size) {
    // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
   function determineDx (elem, size) {
     var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
+    var windowWidth = document.getElementById("#randomPizzas").offsetWidth;
     var oldSize = oldWidth / windowWidth;
 
     // Changes the slider value to a percent width
@@ -449,10 +450,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    for (var i = 0; i < document.getElementsByTagName()(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.getElementsByTagName()(".randomPizzaContainer")[i], size);
+      var newwidth = (document.getElementsByTagName()(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.getElementsByTagName()(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
@@ -502,8 +503,8 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  // items 
-  var items = document.querySelectorAll('.mover');
+  // items is storing all of the pizzas on the page? Because mover is a class name given to the pizzas in the background
+  var items = document.getElementsByTagName()('.mover');
   for (var i = 0; i < items.length; i++) {
     // document.body.scrollTop is no longer supported in Chrome.
 	
@@ -542,7 +543,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    document.getElementById("#movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
